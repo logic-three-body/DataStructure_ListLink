f7971e6df2703954bc037f69194f2de2604b1466
logic-three-body <1552451580@qq.com>
2020-09-22 17:54:17 +0800

    OJ Finish First

--- Reverse_Link/test.cpp
+++ Reverse_Link/test.cpp
@@ -10,15 +10,29 @@ typedef struct LNode
 {
 	ElemType data;
 	LNode *next;
-};
+}LNode, *LinkList;
 
-/*  建立带表头结点的单链线性表L（尾插法） */
-void CreateListTail(LNode *L, int n)
+Status ListTraverse(LinkList& L)
 {
-	LNode* p, *r;
-	int i;                 
-	L = (LNode*)malloc(sizeof(LNode)); /* L为整个线性表 */
-	r = L;                                /* r为指向尾部的结点 */
+	LinkList p = L->next;
+	std::cout << (p->data);
+	p = p->next;
+	while (p)
+	{
+		std::cout << " ";
+		std::cout << p->data;
+		p = p->next;
+	}
+	printf("\n");
+	return OK;
+}
+/*  建立带表头结点的单链线性表L（尾插法） */
+void CreateListTail(LinkList *L, int n)
+{
+	LinkList p, r;
+	int i;
+	*L = (LinkList)malloc(sizeof(LNode)); /* L为整个线性表 */
+	r = *L;                                /* r为指向尾部的结点 */
 	for (i = 0; i < n; i++)
 	{
 		p = (LNode *)malloc(sizeof(LNode)); /*  生成新结点 */
@@ -29,51 +43,44 @@ void CreateListTail(LNode *L, int n)
 	r->next = NULL;                       /* 表示当前链表结束 */
 }
 
-void Reverse_Iteration(LNode*L) //采用迭代头插法逆转链表
+void Reverse_Iteration(LinkList*L) //采用迭代头插法逆转链表
 {
-	if (L->next)//若原链表为空
+	if (!(*L)->next)//若原链表为空
 	{
 		return;
 	}
-	LNode* tmp = nullptr;//中间临时变量，用于存放链表节点
-	LNode* header;//指向链表第一个节点
-	header = (L)->next;
-	(L)->next = nullptr;//置空初始化原链表
+	LinkList tmp = nullptr;//中间临时变量，用于存放链表节点
+	LinkList header;//指向链表第一个节点
+	header = (*L)->next;
+	(*L)->next = nullptr;//置空初始化原链表
 	while (header)//如果此节点存在
 	{
 		tmp = header->next;
-		header->next = (L)->next;//头插法
-		(L)->next = header;//头插法
+		header->next = (*L)->next;//头插法
+		(*L)->next = header;//头插法
 		header = tmp;
 	}
 }
 
-LNode* Reverse_Recursion(LNode*L)//采用递归头插法逆转链表
-{
-	if (!(L)->next)
-	{
-		return (L);
-	}
 
-	//递归翻转子列表
-	LNode* newList = Reverse_Recursion((L)->next);//指向最后一个节点，在递归返回过程中一直指向最后一个节点
-
-	(L)->next->next = (L);//逆向指向前节点，此时当前节点会与前一个节点形成回路
-	(L)->next = nullptr;//断开回路
-	return newList;//成为新的头节点
-}
 
 int main()
 {
-	LNode link;
+	LinkList link;
 	int num;//链表数量
 	std::cin >> num;
-	//尾插法创建链表
-	CreateListTail(&link,num);
-	//迭代头插法反转
-	Reverse_Iteration(&link);
-    link=*Reverse_Recursion(&link);
 
-	system("pause");
+	while (num != 0)
+	{
+		//尾插法创建链表
+		CreateListTail(&link, num);
+		//迭代头插法反转
+		Reverse_Iteration(&link);
+		ListTraverse(link);
+		std::cin >> num;
+	}
+
+
+
 	return 0;
 }
