a08e3fd7e57c1fc9ac78365755a138a85bc0415b
logic-three-body <1552451580@qq.com>
2020-09-29 22:09:38 +0800

    Sth wrong in Merge function

--- Library Creation by linear list/linear_library.cpp
+++ Library Creation by linear list/linear_library.cpp
@@ -28,14 +28,19 @@ public:
 	Status Library_Insert(const std::string &nam, const std::string&num, const double&pric);
 	Status Library_OutPut();
     Status GetLibrary_Len() { return len; };
+	double PriceTag() { return book_price; }
+	Linear_Library(const Linear_Library&);//拷贝构造函数
 }*Lib_conductor;
 int Linear_Library::len=0;
 
-
+//sorting
+void Merge(Lib_conductor SR, Lib_conductor TR, int i, int m, int n);
+void MergePass(Lib_conductor SR, Lib_conductor TR, int s, int n);
+void MergeSort(Lib_conductor L);
 int main()
 {
 	Lib_conductor conductor = new Linear_Library[MAXSIZE];
-	for (int i = 0; i < MAXSIZE; i++)
+	for (int i = 1; i < MAXSIZE; i++)
 	{
 		std::string Book_name;
 		std::string Book_number;
@@ -52,8 +57,9 @@ int main()
 		}
 
 	}
-	std::cout << conductor->GetLibrary_Len() << std::endl;
-	for (int i = 0; i<conductor->GetLibrary_Len(); i++)
+	MergeSort(conductor);
+
+	for (int i = 1; i<conductor->GetLibrary_Len(); i++)
 	{
 		conductor[i].Library_OutPut();
 	}
@@ -62,7 +68,59 @@ int main()
 	return 0;
 }
 
+void Merge(Lib_conductor SR, Lib_conductor TR, int i, int m, int n)
+{
+	int j, k, l;
+	for (j = m + 1, k = i; i <= m && j <= n; k++)	/* 将SR中记录由小到大地并入TR */
+	{
+		if (SR[i].PriceTag() < SR[j].PriceTag())
+			TR[k] = SR[i++];//TODO 拷贝函数靠谱
+		else
+			TR[k] = SR[j++];
+	}
+	if (i <= m)
+	{
+		for (l = 0; l <= m - i; l++)
+			TR[k + l] = SR[i + l];		/* 将剩余的SR[i..m]复制到TR */
+	}
+	if (j <= n)
+	{
+		for (l = 0; l <= n - j; l++)
+			TR[k + l] = SR[j + l];		/* 将剩余的SR[j..n]复制到TR */
+	}
+}
 
+/* 非递归法 */
+/* 将SR[]中相邻长度为s的子序列两两归并到TR[] */
+void MergePass(Lib_conductor SR, Lib_conductor TR, int s, int n)
+{
+	int i = 1;
+	int j;
+	while (i <= n - 2 * s + 1)
+	{/* 两两归并 */
+		Merge(SR, TR, i, i + s - 1, i + 2 * s - 1);
+		i = i + 2 * s;
+	}
+	if (i < n - s + 1) /* 归并最后两个序列 */
+		Merge(SR, TR, i, i + s - 1, n);
+	else /* 若最后只剩下单个子序列 */
+		for (j = i; j <= n; j++)
+			TR[j] = SR[j];
+}
+
+/* 对顺序表L作归并非递归排序 */
+void MergeSort(Lib_conductor L)
+{
+	Lib_conductor TR = new Linear_Library[L->GetLibrary_Len()];/* 申请额外空间 */
+	int k = 1;
+	while (k < L->GetLibrary_Len())
+	{
+		MergePass(L, TR, k, L->GetLibrary_Len());
+		k = 2 * k;/* 子序列长度加倍 */
+		MergePass(TR, L, k, L->GetLibrary_Len());
+		k = 2 * k;/* 子序列长度加倍 */
+	}
+}
 
 Status Linear_Library::Library_Insert(const std::string &nam, const std::string&num, const double&pric)
 {
@@ -77,4 +135,11 @@ Status Linear_Library::Library_OutPut()
 {
 	std::cout<<std::fixed <<std::setprecision(2) << book_number << " " << book_name << " " << book_price<<std::endl;
 	return OK;
+}
+
+Linear_Library::Linear_Library(const Linear_Library &L)
+{
+	book_name = L.book_name;
+	book_number = L.book_number;
+	book_price = L.book_price;
 }
